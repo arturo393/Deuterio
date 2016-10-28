26fdf4aa70e2c2bda35e07b7aa40c39337e62f03
Cactus00 <vagosino@gmail.com>
2016-10-27 22:46:45 -0300

    perdidas en semionductor

---
+++ Codigos/calculo-perdidas.py
@@ -0,0 +1,24 @@
+# -*- coding: latin-1 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""Calculo de perdidas en mosfet, y relaciones con Vth."""
+
+Vgs=10; #Vgs aplicado
+Rds=0.27; #Rds cuando mosfet esta activo [ohm]
+D=0.5; #ciclo de trabajo 
+Vin=50; #Voltage alimentación
+Iout=20; #Corriente salida
+Fsw=100E3; #Frecuencia switching
+tr=58E-9;# tiempo de subida mosfet [ns]
+tf=59E-9; #tiempo de bajada mosfet [ns]
+
+Pc=np.power(Iout,2)*Rds*D ; #Disipación de energía por Isalida
+Psw=0.5*Vin*Iout*(tr+tf)*Fsw; #Disipación de energía por switching
+
+
+
+print "Potencia Continua",Pc,"[W]" 
+print "Potencia Switching",Psw,"[W]" 
+print "Potencia Total",Pc+Psw,"[W]" 
