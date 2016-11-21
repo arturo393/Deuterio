d1bac62ba2d96879e518c6b337b631687a27fe4e
arturo393 <a.veras@gmail.com>
2016-11-21 00:04:31 -0300

    se agrega parametros de identificación del sistema más las ecuaciones

--- Codigos/Modelamiento/sistema_de_segundoorden.py
+++ Codigos/Modelamiento/sistema_de_segundoorden.py
@@ -3,38 +3,35 @@
 import math
 import numpy as np 
 import matplotlib.pyplot as plt
-
 import numpy as np
-
-
-"""
-En el siguiente programa, se obtienen busca obtener los parámetros de un sistema de segundo orden para las celdas de electrólsis
---> Amortiguamiento
--->
--->
--->
--->
--->
-"""
 from scipy import signal
 
-## Parametros de entrada
-u = 12; # Escalon [V]
+u0 = 0;  # nivel inicial de entrada en [V]
+uf = 12.0; # nivel final de entrada en [V]
+y0 = 0;  # nivel inicial de entrada en [A]
+yf = 10.0;  # nivel final de salida en [A]
+A1 = 30.0; # Amplitud del primer pico en [A]
+An = 7.0;  # Amplitud del pico N n en [A]
+Tw = 1e-6; # Tiempo entre dos picos sucesivos [s]
+n = 2 ;   # numero del segundo pico
 
-## Parametros de la respuesta a escalon
-td = 0; # Tiempo de retardo, td
-tr = 0; # Tiempo de levantamiento, tr 
-tp = 0; # Tiempo pico, tp 
-Mp = 0; # Sobrepaso máximo, Mp
-ts = 0; # Tiempo de asentamiento, ts 
-y = 20; # Voltaje máximo de respuesta en [V]
+tmp = 1/(n-1)*np.log(A1/An)
+psi = tmp/np.square(4*np.pi**2 + tmp**2)
+Tn = (Tw/2*np.pi)*np.square(1-psi**2)
+K = (yf-y0)/(uf-u0)
+Wn = 1/Tn;
 
-## Parámetros de la función de transferencia
-K = y/u;   # Ganancia del sistema
-wn = 2; # frecuencua natural
-psi = 0.5; # coeficiente de amortiguamiento relativo 
+print 'Factor de amortiguamiento psi=',psi
+print "Frecuencia natural Wn=",Wn*1e-6, "[MHz]"
+print "Ganancia K=", K
 
-sys2 = signal.lti(K*wn**2, [1, 2*psi*wn, wn**2]) # H(s) = (s + 2) / (s ** 2 + 6 * s + 25)
+sys2 = signal.lti(K, [Tn**2, 2*psi*Tn, 1]) # H(s) = (s + 2) / (s ** 2 + 6 * s + 25)
+
+w, mag, phase = signal.bode(sys2) # Diagrama de bode: frecuencias, magnitud y fase
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 6))
+ax1.semilogx(w, mag) # Eje x logarítmico
+ax2.semilogx(w, phase) # Eje x logarítmico
+
 w, H = signal.freqresp(sys2)
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
 ax1.plot(H.real, H.imag)
@@ -42,5 +39,4 @@ ax1.plot(H.real, -H.imag)
 ax2.plot(sys2.zeros.real, sys2.zeros.imag, 'o')
 ax2.plot(sys2.poles.real, sys2.poles.imag, 'x')
 
-
-
+plt.show()
