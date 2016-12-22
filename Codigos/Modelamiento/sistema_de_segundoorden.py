9ee9de2786ed9514c3339f0df3fbd8b5c458c336
arturo393 <a.veras@gmail.com>
2016-12-21 22:43:40 -0300

    update

--- Codigos/Modelamiento/sistema_de_segundoorden.py
+++ Codigos/Modelamiento/sistema_de_segundoorden.py
@@ -1,19 +1,19 @@
 # -*- coding: latin-1 -*-
 
 import math
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 
 u0 = 0;  # nivel inicial de entrada en [V]
 uf = 12.0; # nivel final de entrada en [V]
-y0 = 0;  # nivel inicial de entrada en [A]
-yf = 10.0;  # nivel final de salida en [A]
-A1 = 30.0; # Amplitud del primer pico en [A]
-An = 7.0;  # Amplitud del pico N n en [A]
-Tw = 1e-6; # Tiempo entre dos picos sucesivos [s]
-n = 2 ;   # numero del segundo pico
+y0 = 0;  # nivel inicial de salida en [A]
+yf = 9.82;  # nivel final de salida en [A]
+A1 = 11.42-yf; # Amplitud del primer pico en [A]
+An = yf-9.71;  # Amplitud del pico N n en [A]
+Tw = (1635-1632)*1e-3; # Tiempo entre dos picos sucesivos [s]
+n = 3 ;   # numero del segundo pico
 
 tmp = 1/(n-1)*np.log(A1/An)
 psi = tmp/np.square(4*np.pi**2 + tmp**2)
@@ -25,18 +25,23 @@ print 'Factor de amortiguamiento psi=',psi
 print "Frecuencia natural Wn=",Wn*1e-6, "[MHz]"
 print "Ganancia K=", K
 
-sys2 = signal.lti(K, [Tn**2, 2*psi*Tn, 1]) # H(s) = (s + 2) / (s ** 2 + 6 * s + 25)
+sys2 = signal.TransferFunction(K, [Tn**2, 2*psi*Tn, 1]) # H(s) = (s + 2) / (s ** 2 + 6 * s + 25)
 
-w, mag, phase = signal.bode(sys2) # Diagrama de bode: frecuencias, magnitud y fase
+w, mag, phase = sys2.bode() # Diagrama de bode: frecuencias, magnitud y fase
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 6))
+plt.grid(True)
+
 ax1.semilogx(w, mag) # Eje x logarítmico
+ax1.set_title('Diagrama de Bode')
 ax2.semilogx(w, phase) # Eje x logarítmico
 
 w, H = signal.freqresp(sys2)
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+plt.grid(True)
 ax1.plot(H.real, H.imag)
 ax1.plot(H.real, -H.imag)
 ax2.plot(sys2.zeros.real, sys2.zeros.imag, 'o')
 ax2.plot(sys2.poles.real, sys2.poles.imag, 'x')
+
 
 plt.show()
